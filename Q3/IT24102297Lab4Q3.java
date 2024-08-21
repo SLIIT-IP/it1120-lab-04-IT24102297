892464648b4363dc294e5a774d3880568682a146
IT24102297 <it24102297@my.sliit.lk>
2024-08-21 23:48:05 +0530

    Java program to Q1 using (? :) to check if the no. is Positive/Negative/Zero.

---
+++ Q3/IT24102297Lab4Q3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+	public class IT24102297Lab4Q3{
+		public static void main(String[] args){
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.print("Enter a number: ");
+			double number= scanner.nextDouble();
+
+			String result= (number>0) ? "The number is:  Positive" :
+					(number<0) ? "The number is: Negative" :
+					"The number is Zero";
+			System.out.println(result);
+
+	}
+}
